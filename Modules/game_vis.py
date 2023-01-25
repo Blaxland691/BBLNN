e3fa4c9b8eed2d16632e404e1716e7f45e430077
wbmcglaughlin <wmcg0001@student.monash.edu>
2023-01-25 13:24:37 +1100

    feature: neural network model added to predict vision

--- Modules/game_vis.py
+++ Modules/game_vis.py
@@ -5,6 +5,9 @@ import pandas as pd
 import seaborn as sns
 import torch
 import Modules.game_set as gs
+from tqdm import tqdm
+
+from torch import nn, optim
 
 
 class PredictNetwork:
@@ -14,6 +17,7 @@ class PredictNetwork:
     def __init__(self, directory):
         self.games = gs.Games(directory)
         self.weights = np.array([3, 3, 1])
+        self.network = Network(6, 2)
 
     def get_train_test_data(self, test_results=30):
         """
@@ -23,7 +27,7 @@ class PredictNetwork:
         """
 
         df_len = len(self.games.game_df)
-        results = np.zeros((df_len - 2, 3))
+        results = np.zeros((df_len - 2, 6))
         winner = []
 
         for i in range(1, df_len - 1):
@@ -31,20 +35,28 @@ class PredictNetwork:
             h_team = self.games.game_df.loc[i]['home_team']
             a_team = self.games.game_df.loc[i]['away_team']
 
-            res = self.get_inputs(df,
-                                         self.games.teams.index(h_team),
-                                         self.games.teams.index(a_team))
+            res_conservative = self.get_inputs(df,
+                                               self.games.teams.index(h_team),
+                                               self.games.teams.index(a_team))
+            res_broad = self.get_inputs(df,
+                                        self.games.teams.index(h_team),
+                                        self.games.teams.index(a_team),
+                                        seasons=4,
+                                        form=10,
+                                        home_form=10)
+
+            res = np.append(res_conservative, res_broad)
             results[i - 1, :] = res
             winner.append(int(self.games.game_df.loc[i]['winner']
                               == self.games.game_df.loc[i]['home_team']))
 
         df = pd.DataFrame(results) - 1
-        df.columns = ['Record', 'Form', 'Home Record']
+        df.columns = ['Record_C', 'Form_C', 'Home Record_C', 'Record', 'Form', 'Home Record']
         df['result'] = winner
 
         return df[:len(df) - test_results], df[len(df) - test_results:]
 
-    def get_inputs(self, df, home_team, away_team):
+    def get_inputs(self, df, home_team, away_team, seasons=2, form=5, home_form=5):
         """
         Get inputs for model.
 
@@ -54,9 +66,9 @@ class PredictNetwork:
         :return: (np.ndarray) array of compiled data.
         """
 
-        record = self.get_record_input(df, home_team, away_team, 2)
-        form = self.get_form_input(df, home_team, away_team, 5)
-        home_record = self.get_home_ground_input(df, home_team, 5)
+        record = self.get_record_input(df, home_team, away_team, seasons)
+        form = self.get_form_input(df, home_team, away_team, form)
+        home_record = self.get_home_ground_input(df, home_team, home_form)
 
         return np.array([record, form, home_record])
 
@@ -176,3 +188,46 @@ class PredictNetwork:
     @staticmethod
     def win_loss_result(wins, losses):
         return wins / (wins + losses) if wins + losses > 0 else 0.5
+
+
+class Network(nn.Module):
+    def __init__(self, inputs: int, outputs: int):
+        layer_one = int(np.sqrt(inputs * outputs))
+        self.hidden_sizes = [layer_one, int(np.max([layer_one / 2, outputs + 1]))]
+
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(inputs, self.hidden_sizes[0]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_sizes[0], self.hidden_sizes[1]),
+            nn.ReLU(),
+            nn.Linear(self.hidden_sizes[1], outputs),
+            nn.LogSoftmax(dim=1)
+        )
+
+        self.criterion = nn.NLLLoss()
+        self.loss = None
+        self.optimizer = optim.SGD(self.parameters(), lr=0.05, momentum=0.5)
+        self.optimizer.zero_grad()
+
+    def train_model(self, x_train, y_train):
+        logps = self.model.forward(x_train)
+        self.loss = self.criterion(logps, y_train)
+
+        epochs = 10000
+        pbar = tqdm(range(epochs))
+
+        for e in pbar:
+            # Training pass
+            self.optimizer.zero_grad()
+            output = self.model.forward(x_train)
+            loss = self.criterion(output, y_train)
+
+            # This is where the model learns by back propagating
+            loss.backward()
+
+            # And optimizes its weights here
+            self.optimizer.step()
+            if e % 100 == 0:
+                pbar.set_postfix_str(f'loss: {loss.item():.2f}')
